676654178ef8f261a51e85e8e06a72c17d57bf4f
aedwardsC <118393110+aedwardsC@users.noreply.github.com>
2024-04-06 11:58:47 -0700

    Basic server listening on local host

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 // This is the main server that the user will connect to and will redirect to the different files
- 
+// use Node.js to run the database
+
+// set up the open source library/framework express for Node.js server
+let express = require('express');
+// start using express
+let app = express();
+
+// listen on localhost: 3000
+app.listen(3000);

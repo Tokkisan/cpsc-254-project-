1edf526d98494bf10acd2674014c9b16d8784854
Aaron-k-lee <163605701+Aaron-k-lee@users.noreply.github.com>
2024-04-15 22:17:10 -0700

    update

--- database_functions.js
+++ database_functions.js
@@ -1,13 +1,23 @@
 function addAccount(email, password) {
     // add the email and password to the database
-    send("Account has been added");
+    console.log("Account has been added");
 }
 
 function retrieveAccount(email) {
     // search the database by the email provided
-    send("Verified");
+    console.log("Verified");
     // return the password from the table
+}
+
+function addToDo(toDoReminder) {
+    // add the reminder to the database
+}
+
+function retrieveToDoReminders() {
+    // retrieve all of the reminders from the database for displaying
 }
 
 module.exports = {addAccount};
 module.exports = {retrieveAccount};
+module.exports = {addToDo};
+module.exports = {retrieveToDoReminders};

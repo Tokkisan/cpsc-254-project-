2292d96c6e0a3b7d91b3662779b5eb49489ee867
Jtamara <70074848+Jtamara@users.noreply.github.com>
2024-05-07 17:44:57 -0700

    Update updatePackages.sh

--- updatePackages.sh
+++ updatePackages.sh
@@ -10,7 +10,8 @@ sudo apt-get update
 # Log the date
 # This is for logging and what needed to be updated
 date >> updatePackagesLog.txt
-apt list --upgradable >> updatePackagesLog.txt
+#NOTE fix the directory for this
+apt list --upgradable >> /home/jt/updatePackagesLog.txt
 
 # Upgrade installed packages and removes packages that can no longer be downloaded
 sudo apt-get upgrade
